b73ae19f8efde3d8e4f7497a6b5b9db68b53d8d6
Himanshu Pareek <himanshupareekiit01@gmail.com>
2026-06-19 21:52:26 +0530

    Use existing JdbcRegisteredClientRepository

--- src/main/java/dev/javarush/youtube/auth_server/client/ClientConfig.java
+++ src/main/java/dev/javarush/youtube/auth_server/client/ClientConfig.java
@@ -1,13 +1,13 @@
 package dev.javarush.youtube.auth_server.client;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
-@EnableJdbcRepositories
 public class ClientConfig {
     @Bean
-    RegisteredClientRepository clientRepository(ClientEntityRepository entityRepository) {
-        return new CustomJdbcRegisteredClientRepository(entityRepository);
+    RegisteredClientRepository clientRepository(JdbcOperations jdbcOperations) {
+        return new JdbcRegisteredClientRepository(jdbcOperations);
     }
 }
