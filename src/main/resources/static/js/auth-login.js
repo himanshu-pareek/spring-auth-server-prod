eda82e85a902cf486563d73130d8b88a6d04cc55
Himanshu Pareek <himanshupareekiit01@gmail.com>
2026-07-04 21:39:42 +0530

    Remove unnecessary logs

--- src/main/resources/static/js/auth-login.js
+++ src/main/resources/static/js/auth-login.js
@@ -3,17 +3,18 @@ import { authenticateWithPassKey } from "./webauthn.js"
 document.addEventListener("DOMContentLoaded", () => {
     const passKeySignInForm = document.getElementById("passkey-login-form")
 
+    if (!window.PublicKeyCredential) {
+        passKeySignInForm.remove()
+    }
+
     passKeySignInForm.addEventListener("submit", async (event) => {
-        console.log("Handling submitting...")
         event.preventDefault()
-        console.log(event)
 
         const formData = new FormData(event.target)
         const formEntries = Object.fromEntries(formData)
 
         try {
             if (!window.PublicKeyCredential) {
-                // TODO: Hide passkey sign in button
                 window.alert('WebAuthn is not supported')
                 return
             }
@@ -24,7 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (result.authenticated) {
                 if (result.redirectUrl) {
-                    window.alert(`Authenticated. Will redirect to ${result.redirectUrl}`)
                     window.location.href = result.redirectUrl
                 } else {
                     window.alert('Authentication is successful')
