6bfdb2a580823e8b329e83e58328744630ecc9b1
Himanshu Pareek <himanshupareekiit01@gmail.com>
2026-07-04 22:24:34 +0530

    Add support for webauthn autofill

--- src/main/resources/static/js/auth-login.js
+++ src/main/resources/static/js/auth-login.js
@@ -1,39 +1,72 @@
-import { authenticateWithPassKey } from "./webauthn.js"
+import { getAuthenticationOptions, verifyCredential } from './api.js'
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const passKeySignInForm = document.getElementById("passkey-login-form")
 
-    if (!window.PublicKeyCredential) {
-        passKeySignInForm.remove()
+    const { browserSupportsWebAuthn } = SimpleWebAuthnBrowser
+
+    if (!browserSupportsWebAuthn()) {
+        passKeySignInForm.style.display = 'none'
+        return
     }
+
+    const csrfToken = getCsrfToken(passKeySignInForm)
+    if (!csrfToken) {
+        passKeySignInForm.style.display = 'none'
+        return
+    }
+
+    const headers = { 'X-CSRF-TOKEN': csrfToken }
+    const authenticationOptions = await getAuthenticationOptions(headers)
+
+    signInUsingPassKey(authenticationOptions, headers, true)
 
     passKeySignInForm.addEventListener("submit", async (event) => {
         event.preventDefault()
 
-        const formData = new FormData(event.target)
-        const formEntries = Object.fromEntries(formData)
-
-        try {
-            if (!window.PublicKeyCredential) {
-                window.alert('WebAuthn is not supported')
-                return
-            }
-
-            const result = await authenticateWithPassKey({
-                'X-CSRF-TOKEN': formEntries['_csrf']
-            })
-
-            if (result.authenticated) {
-                if (result.redirectUrl) {
-                    window.location.href = result.redirectUrl
-                } else {
-                    window.alert('Authentication is successful')
-                }
-            } else {
-                window.alert('Authentication failed. Please try again.')
-            }
-        } catch (error) {
-            window.alert('Error occurred: ' + error.message)
+        if (!window.PublicKeyCredential) {
+            window.alert('WebAuthn is not supported')
+            return
         }
+
+        signInUsingPassKey(authenticationOptions, false)
     })
 })
+
+const signInUsingPassKey = async (authenticationOptions, headers, autoFill) => {
+    try {
+        const { startAuthentication } = window.SimpleWebAuthnBrowser;
+
+        if (!startAuthentication) {
+            throw new Error('WebAuthn library not loaded properly')
+        }
+
+
+        const authenticationResponse = await startAuthentication({ optionsJSON: authenticationOptions, useBrowserAutofill: autoFill })
+
+        const result = await verifyCredential(authenticationResponse, headers)
+
+        if (result.authenticated) {
+            if (result.redirectUrl) {
+                window.location.href = result.redirectUrl
+            } else {
+                window.alert('Authentication is successful')
+            }
+        } else {
+            window.alert('Authentication failed. Please try again.')
+        }
+    } catch (error) {
+        console.error('Authentication error: ', error)
+        if (error.name == 'NotAllowedError') {
+            throw new Error('Authentication was cancelled or timed out')
+        } else {
+            throw new Error(`Failed to authenticate with PassKey: ${error.message}`)
+        }
+    }
+}
+
+const getCsrfToken = (formElement) => {
+    const formData = new FormData(formElement)
+    const formEntries = Object.fromEntries(formData)
+    return formEntries['_csrf']
+}
